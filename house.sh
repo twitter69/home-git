c77e5f8972a7d97633c11ac06442623cd5a833c8
Uflorence30 <118619117+Uflorence30@users.noreply.github.com>
2023-02-06 20:30:54 -0600

    Update house.sh
    More upgrade will be added as we go. Grace has found me.

--- house.sh
+++ house.sh
@@ -1,1 +1,12 @@
 God is blessing me with a new house in 2023
+high ceiling
+contemporary elevation
+2 story
+5 bedrooms
+5 bath
+spiral staircase
+master down
+guestroom down
+gameroom/mediaroom up
+4 car garage
+victorian flooring
